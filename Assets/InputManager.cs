4d44fb15f87527955b3dc419672af62d78f56b73
Tomáš Isteník <epholl@gmail.com>
2015-07-03 17:14:34 +0200

    Added basic key input, menu and screen output.

---
+++ Assets/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	public Camera mainCamera;
+
+	void FixedUpdate () {
+
+		float x = mainCamera.transform.position.x;
+		float y = mainCamera.transform.position.y;
+		float z = mainCamera.transform.position.z;
+	
+		if (Input.GetKey ("up") || Input.GetKey("w")) {
+			z++;
+		}
+		if (Input.GetKey ("down") || Input.GetKey("s")) {
+			z--;
+		}
+		if (Input.GetKey ("left") || Input.GetKey("a")) {
+			x--;
+		}
+		if (Input.GetKey ("right") || Input.GetKey("d")) {
+			x++;
+		}
+
+		mainCamera.transform.position = new Vector3(x, y, z);
+	}
+}
---
+++ Assets/ScreenDataActualizer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScreenDataActualizer : MonoBehaviour {
+
+	public Camera mainCamera;
+
+	public Text textX;
+	public Text textZ;
+	public Text textY;
+	public Text textFOV;
+
+	// Update is called once per frame
+	void Update () {
+
+		textX.text = "Offset X: " + mainCamera.transform.position.x;
+		textZ.text = "Offset Z: " + mainCamera.transform.position.z;
+		textY.text = "Altitude: " + mainCamera.transform.position.y;
+		textFOV.text = "FOV: " + mainCamera.fieldOfView;
+	}
+}
